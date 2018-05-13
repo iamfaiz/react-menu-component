8ddafb47370eccf04e78c65223788d55b253f5e5
Rana Faiz Ahmad <iamfaizahmed123@gmail.com>
2018-05-13 09:31:20 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import EpicMenu from './EpicMenu';
+
+import logo from './logo.png';
+
+class App extends Component {
+  render() {
+    let links = [
+      { label: 'Home', link: '#home', active: true },
+      { label: 'About', link: '#about' },
+      { label: 'Portfolio', link: '#portfolio' },
+      { label: 'Contact Us', link: '#contact-us' },
+    ];
+
+    return (
+      <div className="container center">
+        <EpicMenu links={links} logo={logo} />
+      </div>
+    );
+  }
+}
+
+export default App;
